23e44559e4aab496061ab7f1daac55ade5a2eb20
Koji-Desu <pdigo153@gmail.com>
2022-04-26 00:48:26 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import NavBar from './components/NavBar';
+import BemVindo from './components/BemVindo';
+
 function App() {
+  const caminhos = [
+    {
+      id: 1,
+      caminho: '/',
+      titulo: 'Página inicial'
+    },
+    {
+      id: 2,
+      caminho: '/perfil',
+      titulo: 'Perfil pessoal'
+    },
+    {
+      id: 3,
+      caminho: '/repositorios',
+      titulo: 'Repositórios'
+    },
+    {
+      id: 4,
+      caminho: '/busca',
+      titulo: 'Busca'
+    }
+    
+  ]
   return (
-    <div className="App">
-     
+    <div >
+      <NavBar menu={caminhos} />
+      <BemVindo />
     </div>
   );
 }
